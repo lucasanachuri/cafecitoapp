d708d215f834c21dccd07a130995ec5e820528b2
[LucasA] <[lucas.anachuri@gmail.com]>
2023-07-31 21:37:18 -0300

    cafecito v1

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,18 +1,28 @@
 import { Navbar, Container, Nav } from "react-bootstrap";
-
+import { NavLink, Link } from "react-router-dom";
 
 const Menu = () => {
   return (
     <Navbar bg="danger" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Cafecito</Navbar.Brand>
+        <Navbar.Brand as={Link} to={"/"}>
+          Cafecito
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="#">Inicio</Nav.Link>
-            <Nav.Link href="#">Registro</Nav.Link>
-            <Nav.Link href="#">Administrador</Nav.Link>
-            <Nav.Link href="#">Login</Nav.Link>
+            <NavLink end to="/" className="nav-link">
+              Inicio
+            </NavLink>
+            <Nav.Link end to="registro" className="nav-link">
+              Registro
+            </Nav.Link>
+            <Nav.Link end to="administrador" className="nav-link">
+              Administrador
+            </Nav.Link>
+            <Nav.Link end to="login" className="nav-link">
+              Login
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
